445dca7c68dd932bbc14a993b0873f67ef0c7589
iansim1 <iansimlikyong50@gmail.com>
2024-10-03 21:55:12 -0700

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,9 @@
+# Signature: str -> str
+# Purpose: So that "Hello World!" would be at the beginning
+def hello_world(x):
+    return "Hello World!" + x
+
+# Test
+print(hello_world(" Good Morning"))
+print(hello_world(" CPSC110"))
 # Answer Problem 1 here
